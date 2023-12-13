8e22e62d4a97e25c0f516350a757dde78c060a83
Sina6464 <149200885+Sina6464@users.noreply.github.com>
2023-12-13 15:13:11 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of rows for the triangle pattern: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
